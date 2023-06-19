b40be2b45a5c040cec1630ca7889b219f1861f04
Valeriy Pogorelov <valeriy.pogorelov@together.com>
2023-06-19 13:22:02 +0300

    Remove dev pallete

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,6 @@ import AuthZone from "../components/layout/AuthZone";
 import {ThemeProvider} from "@mui/material";
 import {createTheme} from "@mui/material/styles";
 import client from "../graphql/client";
-import {DevSupport} from "@react-buddy/ide-toolbox";
 import {ComponentPreviews, useInitial} from "../dev";
 
 const theme = createTheme();
@@ -16,11 +15,7 @@ export default function App({Component, pageProps}) {
       <ApolloProvider client={client}>
         <ThemeProvider theme={theme}>
           <AuthZone>
-            <DevSupport ComponentPreviews={ComponentPreviews}
-                        useInitialHook={useInitial}
-            >
               <Component {...pageProps}/>
-            </DevSupport>
           </AuthZone>
         </ThemeProvider>
       </ApolloProvider>
